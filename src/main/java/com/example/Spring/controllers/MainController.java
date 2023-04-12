f6bb38ab3c4f5de870978bd517d06d348000157f
Сергей Куликов <sergokulik61@gmail.com>
2023-04-12 21:44:55 +0400

    Updated project (leaving comments added)

--- src/main/java/com/example/Spring/controllers/MainController.java
+++ src/main/java/com/example/Spring/controllers/MainController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -28,11 +31,19 @@ public class MainController {
         return "main";
     }
     @GetMapping("/{id}")
-    public String postView(@PathVariable Long id, Model model){
+    public String postView(@PathVariable Long id, Model model) {
         Post post = postRepo.findById(id).orElse(null);
         List<Comment> comments = commentRepo.findByPost(post);
         model.addAttribute("post", post);
         model.addAttribute("comments", comments);
         return "post";
+    }
+
+    @PostMapping("/{id}")
+    public String leaveComment(@PathVariable Long id, @RequestParam String name, @RequestParam String text,
+                              Model model){
+        Comment comment = new Comment(name,text, LocalDate.now(),postRepo.findById(id).orElse(null));
+        commentRepo.save(comment);
+        return "redirect:/{id}";
     }
 }
